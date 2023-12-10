cdc4557dcaefaca71d3c45a14b0db270083a4a6f
ralmuetecentennialgit <ralmuete@my.centennialcollege.ca>
2023-12-09 20:24:27 -0500

    Implement PUT customer method to update customer record

---
+++ RiceLinkAPI/Models/Customer/UpsertCustomerRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RiceLinkAPI.Models.Customer
+{
+    public class UpsertCustomerRequest
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+        public string Email { get; set; }
+        public string Phone { get; set; } 
+        public string? Company { get; set; } 
+        public string Address { get; set; }
+    }
+}
+
