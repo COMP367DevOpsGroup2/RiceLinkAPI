ed758f2bb647dea1fca7e0d6b4232d37d46a5a96
ralmuetecentennialgit <ralmuete@my.centennialcollege.ca>
2023-12-09 21:47:35 -0500

    Fix email and phone validation in PATCH method of customer endpoints

--- RiceLinkAPI/Models/Customer/PatchCustomerRequest.cs
+++ RiceLinkAPI/Models/Customer/PatchCustomerRequest.cs
@@ -14,16 +14,9 @@ namespace RiceLinkAPI.Models.Customer
  
         [StringLength(50, ErrorMessage = "LastName size must be less than 50 characters.")]
         public string? LastName { get; set; }
-
-        [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string? Email { get; set; }
         public string? Phone { get; set; }
-
-        [Required(ErrorMessage = "Phone is required.")]
-        [Phone(ErrorMessage = "Invalid phone number format.")]
-        [StringLength(50, ErrorMessage = "Company must be less than 50 characters.")]
-        public string? Company { get; set; }
+       public string? Company { get; set; }
 
     
         [StringLength(200, ErrorMessage = "Address must be less than 200 characters.")]
